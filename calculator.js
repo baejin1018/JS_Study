a5b976c9769d5e0a0964bd44861346af36347c8c
Bae-Jin-Young <bae20051018@gmail.com>
2021-08-20 18:22:30 +0900

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const calculator ={
+    add: function(a,b){
+        console.log(a+b);
+    },
+    minus: function(a,b){
+        console.log(a-b);
+    },
+    divide: function(a,b){
+        console.log(a/b);
+    },
+    mul: function(a,b){
+        console.log(a*b);
+    },
+    pow:function(a,b){
+        console.log(a**b);
+    }
+};
+
+calculator.add(5,1);
+calculator.minus(6,3);
+calculator.divide(8,2);
+calculator .mul(5,3);
+calculator.pow(5,2);
