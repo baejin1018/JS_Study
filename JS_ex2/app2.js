188a4bcb00215fb8acc702f2fa6b62ed7f3cf5ae
Bae-Jin-Young <bae20051018@gmail.com>
2021-09-02 20:52:58 +0900

    nomad_chrome_app4.4

--- JS_ex2/app2.js
+++ JS_ex2/app2.js
@@ -1,10 +1,15 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput =  document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+
 function onLoginSubmit(event){
     event.preventDefault();//브라우져의 기본동작을 멈춤
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);//Log In 버튼을 누르면 form 을 숨김
+    const username = loginInput.value;
+    greeting.innerText = "Hello" + username;//'Hello ${username}`;=="Hello" + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
