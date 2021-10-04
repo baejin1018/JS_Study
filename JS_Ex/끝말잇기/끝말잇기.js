79aa110b544b9f0870e4467a0f9c2738aead930e
Bae-Jin-Young <bae20051018@gmail.com>
2021-10-04 21:56:54 +0900

    Study : js endwordgame finish
    제로초 강의를 듣고 만든 끝말잇기

--- JS_Ex/끝말잇기/끝말잇기.js
+++ JS_Ex/끝말잇기/끝말잇기.js
@@ -9,31 +9,27 @@ const order = document.querySelector("#order");
 let word; //제시어
 let newWord; //새로입력한 단어
 
+document.querySelector("#players").textContent = playerNum;
+
 inputText.addEventListener("input", (event) => {
   newWord = event.target.value;
 });
 
 onClickButton.addEventListener("click", () => {
-  if (!word) {
-    //제시어가 비어있는 경우
+  if (!word || word[word.length - 1] === newWord[0]) {
+    //제시어가 비어있는 경우 or 끝글자와 새글자의 첫 글자가 같으면
     word = newWord;
     pWord.textContent = word;
-    input.value = ""; //입력칸안의 내용을 지움
-
     const mOrder = Number(order.textContent); //현재순서
-    if(order+1 = playerNum)){
-
-    }else{
-
+    if (mOrder + 1 > playerNum) {
+      //현재 순서가 참가 인원보다 많으면
+      order.textContent = 1; // 순서를 1로 변경
+    } else {
+      order.textContent = mOrder + 1; //현재 순서 +1
     }
   } else {
-    //비어있지 않은 경우
-    if (word[word.length - 1] === newWord[0]) {
-      // 제시어의 끝글자가 새로운 글자의 첫글자와 같은지
-      word = newWord;
-      pWord.textContent = word;
-      input.value = ""; //입력칸안의 내용을 지움
-    } else {
-    }
+    alert("올바르지 않은 단어 입니다");
   }
+  inputText.value = ""; //입력칸안의 내용을 지움
+  inputText.focus();
 });
