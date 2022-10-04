15ea441be0e7455f2a6e3bf8093a9652d8c55a30
진영 <bae20051018@gmail.com>
2022-10-04 23:11:05 +0900

    Study : 2-2 node

--- 2-2/node실습/test/routes/info.js
+++ 2-2/node실습/test/routes/info.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.render("info", { title: "info", message: "hi" });
 });
 
+router.post("/update", (req, res) => {
+  const title = req.body.title;
+  res.send(`Title is ${title}`);
+});
 module.exports = router;
--- 2-2/node실습/test/routes/users.js
+++ 2-2/node실습/test/routes/users.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
+  const message = ["Hello", "Everyone", "!"];
   const users = [
     { id: 1, name: "배" },
     { id: 2, name: "진" },
     { id: 3, name: "영" },
   ];
-  res.json(users);
+  res.render("users", { message: message, users: users });
 });
 
+router.get("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  res.send(`Hello ${userId}`);
+});
 module.exports = router;
