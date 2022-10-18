cef3152202d6712776eec4affaba27c73c76aa2d
진영 <bae20051018@gmail.com>
2022-10-18 15:34:23 +0900

    Study : 2-2 node class
    todolist

---
+++ 2-2/node실습/todos/models/sqlite/todo-sql.js
@@ -0,0 +1,15 @@
+exports.createTodoTable = () => {
+  return `create table if not exists todo(todoId integer primary key autoincrement, job text)`;
+};
+
+exports.insertTodo = (job) => {
+  return `insert into todo(job) values( '${job}' )`;
+};
+
+exports.selectTodos = () => {
+  return `select * from todo`;
+};
+
+exports.deleteTodo = (todoId) => {
+  return `delete from todo where todoId=${todoId}`;
+};
