e3ca33fa389f5d7d3b626d30dc06dd09f78a1bb1
진영 <bae20051018@gmail.com>
2022-10-25 16:04:30 +0900

    Study : 2-2 sequelize
    2-2 웹프 시간에 배운 sequelize

--- 2-2/node실습/todos/routes/index.js
+++ 2-2/node실습/todos/routes/index.js
@@ -1,17 +1,32 @@
 var express = require("express");
-const db = require("../models/sqlite/db");
-const sql = require("../models/sqlite/todo-sql");
 
+//sqlLite
+// const db = require("../models/sqlite/db");
+// const sql = require("../models/sqlite/todo-sql");
+
+//MariaDB
+// const tododb = require("../models/mairadb/todo");
+
+//Sequelize
+const Todo = require("../models/sequelize/todoModel");
 const router = express.Router();
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  db.executeQuery(sql.selectTodos(), (err, rows) => {
-    if (err) {
-      res.render("index");
-    } else {
-      res.render("index", { todos: rows });
-    }
-  });
+//SQLLite
+// router.get("/", function (req, res, next) {
+//   db.executeQuery(sql.selectTodos(), (err, rows) => {
+//     if (err) {
+//       res.render("index");
+//     } else {
+//       res.render("index", { todos: rows });
+//     }
+//   });
+// });
+
+//MariaDB
+router.get("/", async (req, res) => {
+  // const rows = await tododb.selectTodos();
+  const rows = await Todo.findAll();
+  res.render("index", { todos: rows });
 });
 
 module.exports = router;
