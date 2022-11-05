b3b56e642a6406fd4d430899c59972d21f5cd240
진영 <bae20051018@gmail.com>
2022-11-05 22:43:08 +0900

    Study : 2-2 formExam

--- 2-2/formCheckExam/index2.js
+++ 2-2/formCheckExam/index2.js
@@ -1,3 +1,31 @@
+const form = document.getElementsByTagName("form");
+
+const onClickImm = () => {
+  if (checkForm()) {
+    const result = confirm("즉시등록 하시겠습니까?");
+    if (result) {
+      alert("등록되었습니다.");
+    } else {
+      alert("취소되었습니다.");
+      return true;
+    }
+    form[0].submit();
+  }
+};
+
+const onClickSave = () => {
+  if (checkForm()) {
+    const result = confirm("임시저장 하시겠습니까?");
+    if (result) {
+      alert("저장되었습니다.");
+    } else {
+      alert("취소되었습니다.");
+      return true;
+    }
+    form[0].submit();
+  }
+};
+
 function checkForm() {
   const name = document.getElementById("상호");
   const businessNum = document.getElementById("사업자등록번호");
@@ -5,9 +33,7 @@ function checkForm() {
   const account = document.getElementById("거래처ID");
   const pw = document.getElementById("비밀번호");
   const pwCheck = document.getElementById("pwCheck");
-  const classification = document.getElementById("classification");
-  // const save = document.getElementById("save");
-  // const selected = document.querySelector('input[name="personInfo"]:checked');
+  const selected = document.querySelector('input[name="personInfo"]:checked');
   const none = document.getElementsByTagName("p");
   const formInfo = [
     name,
@@ -15,27 +41,65 @@ function checkForm() {
     phoneNum,
     account,
     pw,
-    // classification,
-    // selected,
+    pwCheck,
+    selected,
   ];
-  console.log(formInfo);
+
+  const setClassOk = (i) => {
+    none[i].className = "ok";
+    return true;
+  };
+
+  const setClassNone = (i) => {
+    none[i].className = "none";
+    return false;
+  };
+
+  const setEvent = (item, i, event) => {
+    item.addEventListener("change", (e) => event(e, i));
+  };
+
+  const firstCheck = (item, i) => {
+    return item.value ? setClassOk(i) : setClassNone(i);
+  };
+  const checkEvent = (e, i) => {
+    e.target.value.length != 0 ? setClassOk(i) : setClassNone(i);
+  };
+
+  const onChangePwCheck = (e) => {
+    e.target.value == pw.value ? setClassOk(5) : setClassNone(5);
+  };
+
+  const checkPassword = (i) => {
+    if ((pwCheck.value !== pw.value) | (pwCheck.value == "")) {
+      return setClassNone(i);
+    } else {
+      return setClassOk(i);
+    }
+  };
 
   const check = {
-    false(tag, name, i) {
-      none[i].className = "none";
-      return false;
+    same(item, i) {
+      setEvent(item, i, checkEvent);
+      return firstCheck(item, i);
     },
-    true(x, j, i) {
-      none[i].className = "ok";
-      return true;
+    pwCheck(item, i) {
+      setEvent(item, i, onChangePwCheck);
+      return checkPassword(i);
+    },
+    undefined(_, i) {
+      return setClassNone(i);
+    },
+    personInfo(item, i) {
+      if ((item.value == "undefined") | item) {
+        return setClassNone(i);
+      } else return setClassOk(i);
     },
   };
 
-  const checkResult = formInfo.map((item, i) =>
-    check[Boolean(item.value)](item, item.id, i)
-  );
-
-  console.log("CK", checkResult);
+  const checkResult = formInfo.map((item, i) => {
+    return check[item?.name](item, i);
+  });
 
   return !checkResult.includes(false);
 }
